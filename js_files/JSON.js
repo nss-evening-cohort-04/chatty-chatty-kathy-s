c75d7b3dccb1b543baa24e5772ae6245303f039b
Alex Saber <Alex Saber>
2016-09-10 13:17:35 -0500

    added XHR to console.log message array

---
+++ js_files/JSON.js
@@ -0,0 +1,14 @@
+var Chatty = (function() {
+
+  function getMessages() {
+    var data = JSON.parse(this.responseText);
+    var messagesArray = data.messages;
+    console.log(messagesArray);
+  }
+
+
+  var request = new XMLHttpRequest();
+  request.addEventListener("load",getMessages);
+  request.open("GET","messages.json");
+  request.send();
+})();
