1c4293164f6b4909d85f559753e753234253bcab
Alex Saber <Alex Saber>
2016-09-16 18:05:32 -0500

    changed dark theme border color to skyblue for consistency; adjusted logo text to be more centered

--- js_files/removeMessage.js
+++ js_files/removeMessage.js
@@ -2,6 +2,7 @@ var Chatty = (function(Chatty) {
 
   Chatty.remove = function (event) {
     var clicked = event;
+    console.log(clicked.parentElement);
     container.removeChild(clicked.parentElement);
     if (document.getElementById("container").innerHTML < 1) {
       document.getElementById("clearButton").disabled = true;
