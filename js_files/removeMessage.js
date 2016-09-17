644ae819dee9b8b3558888ca9a703127908c9103
Alex Saber <Alex Saber>
2016-09-17 02:05:26 -0500

    utilized for loop to search array for deleted DOM message, then remove the entire object

--- js_files/removeMessage.js
+++ js_files/removeMessage.js
@@ -1,8 +1,21 @@
 var Chatty = (function(Chatty) {
 
   Chatty.remove = function (event) {
+    //first, remove clicked element from the DOM
+    var currentMessagesArray = Chatty.getMessagesArray();
     var clicked = event;
     container.removeChild(clicked.parentElement);
+
+    //then, remove clicked object from the messagesArray
+    var clickedID = clicked.parentElement.id;
+    for(var i = 0; i < currentMessagesArray.length; i++) {
+      if(currentMessagesArray[i].id == clickedID) {
+        currentMessagesArray.splice(i, 1);
+        console.log(currentMessagesArray);
+        break;
+      }
+    }
+    //if the messages container div is empty, disable the Clear All Messages button
     if (document.getElementById("container").innerHTML < 1) {
       document.getElementById("clearButton").disabled = true;
     }
